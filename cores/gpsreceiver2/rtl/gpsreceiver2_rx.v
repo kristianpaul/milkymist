653968b3d67aad978c5266d8ccb9b2c1934816d7
Cristian Paul Peñaranda Rojas <paul@kristianpaul.org>
2011-06-22 00:44:25 -0500

    Code clean-up, just shifter

--- cores/gpsreceiver2/rtl/gpsreceiver2_rx.v
+++ cores/gpsreceiver2/rtl/gpsreceiver2_rx.v
@@ -17,111 +17,46 @@
  */
 
 module gpsreceiver2_rx(
-	output reg [10:0] rx_count_0,
 
-	output [7:0] rxb0_dat,
-	output [10:0] rxb0_adr,
-	output rxb0_we,
-	
+	/* from gps front end */
 	input gps_rec_clk,
 	input gps_rec_sync,
 	input gps_rec_data,
 	
+	output rxb0_clk,
+	output [7:0] rxb0_dat,
+
 	output gps_led
+
 );
-/*   Data Stream
-*    from SiGE SE4162T
-*
-	* Nibble
-	* SMSM  SMSMSMSMSMSMSMSMSMSMSMSMSMSM SM SM
-	* IIQQ  IIQQIIQQIIQQIIQQIIQQIIQQIIQQ II QQ 
-	* S     S   S   S   S   S   S   S    S       Sync
-	* CCCC  CCCCCCCCCCCCCCCCCCCCCCCCCCCC CC CC   Clock
-	*/
-
 /* Serial to Parallel */
-reg [3:0] iqnibble_rx_data;
-always @(posedge gps_rec_clk)
-begin
-	iqnibble_rx_data <= {gps_rec_data,iqnibble_rx_data[3:1]};
+reg [7:0] iq_rx_data;
+always @(posedge gps_rec_clk) begin
+	iq_rx_data <= {gps_rec_data,iq_rx_data[7:1]};
 end
 
-/* Address Counter */
-always @(posedge gps_rec_clk) begin
-	if(gps_rec_clk)
-		rx_count_0 <= rx_count_0 + 11'd0;
+assign rxb0_dat = iq_rx_data;
+
+/* clock pulse for byte sync */
+reg sync_counter = 1'b0;
+always @(posedge gps_rec_sync) begin
+	sync_counter <= sync_counter + 1;
+end
+
+reg sync_clk;
+always @(gps_rec_clk) begin
+	if(~sync_counter & gps_rec_sync)
+		sync_clk = 1'b1;
 	else
-		rx_count_0 <= rx_count_0 + 11'd1;
+		sync_clk = 1'b0;
 end
 
-assign rxb0_adr = rx_count_0;
+assign  rxb0_clk = sync_clk;
 
-/* Data loading into buffer */
-reg rxb_we_ctl;
-assign rxb0_we = rxb_we_ctl;
 
-reg [3:0] lo;
-reg [3:0] hi;
-reg [1:0] load_nibble;
-/* concatenate two nibble as one byte */
-always @(posedge gps_rec_clk) begin
-	if(load_nibble[0])
-		lo <= iqnibble_rx_data;
-	if(load_nibble[1])
-		hi <= iqnibble_rx_data;
-end
-assign rxb0_dat = {hi, lo};
-
-reg [1:0] state;
-reg [1:0] next_state;
-
-parameter IDLE		= 2'd0;
-parameter LOAD_LO	= 2'd1;
-parameter LOAD_HI	= 2'd2;
-parameter TERMINATE	= 2'd3;
-
-initial state <= IDLE;
-always @(posedge gps_rec_clk)
-	state <= next_state;
-
-always @(*) begin
-	rxb_we_ctl = 1'b0;
-	load_nibble = 2'b00;
-	
-	next_state = state;
-	case(state)
-		IDLE: begin
-			if(gps_rec_sync) begin
-				load_nibble = 2'b01;
-				next_state = LOAD_HI;
-			end
-		end
-		LOAD_LO: begin
-			rxb_we_ctl = 1'b1;
-			if(gps_rec_sync) begin
-				load_nibble = 2'b01;
-				next_state = LOAD_HI;
-			end else begin
-				next_state = TERMINATE;
-			end
-		end
-		LOAD_HI: begin
-			if(gps_rec_sync) begin
-				load_nibble = 2'b10;
-				next_state = LOAD_LO;
-			end else begin
-				next_state = TERMINATE;
-			end
-		end
-		TERMINATE: begin
-			next_state = IDLE;
-		end
-	endcase
-end
-
-/* Debug */
+/* Silly  Debug */
 reg [22:0]  counter = 23'b0;
-always @(posedge gps_rec_clk) begin
+always @(posedge rxb0_clk) begin
 	counter <= counter + 1;
 end
 assign gps_led = counter[21];
